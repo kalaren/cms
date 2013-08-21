96133a4dc7cb7611a7c631c1ff32e2313bd3ab3a
Eric Szeto <eszeto@gmail.com>
2013-08-21 14:49:35 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TradeBoard::Application.config.secret_token = 'cb525ba3e878d450054b4766371f067004bb01fa0ff2dedf33ff0971e6f2b59102a8dfa3f562f9b911189382a74df2d90c451ce39ae5fc6930b61d7ca11ada7d'
